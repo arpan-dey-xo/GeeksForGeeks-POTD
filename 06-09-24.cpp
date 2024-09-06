c16c9f0d33c3d601c48830f8de9573c6b0abfdc6
Arpan Dey <midecl.arpan@gmail.com>
2024-09-06 21:29:58 +0530

    Kadane's Algorithm

---
+++ 06-09-24.cpp
@@ -0,0 +1,13 @@
+class Solution {
+  public:
+    int maxSubarraySum(vector<int> &arr) {
+        int kadane=INT_MIN,count=0;
+        for(int i=0;i<arr.size();i++) {
+            count+=arr[i];
+            kadane=max(kadane,count);
+            if(count<0)
+            count=0;
+        }
+        return kadane;
+    }
+};
