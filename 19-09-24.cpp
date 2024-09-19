864cc0c7d432d67296975e5c4f6b5aa31be205f8
Arpan Dey <midecl.arpan@gmail.com>
2024-09-19 21:06:11 +0530

    Reverse Words

---
+++ 19-09-24.cpp
@@ -0,0 +1,15 @@
+class Solution {
+  public:
+    string reverseWords(string str) {
+        int j=0;
+        for(int i=0;i<str.length();i++){
+            if(str[i]=='.'){
+                reverse(str.begin()+j,str.begin()+i);
+                j=i+1;
+            }
+            else if(i==str.length()-1)reverse(str.begin()+j,str.end());
+        }
+        reverse(str.begin(),str.end());
+        return str;
+    }
+};
