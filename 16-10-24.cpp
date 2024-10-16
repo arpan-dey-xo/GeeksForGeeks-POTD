9f93a92d06f58ffda22e63edbb2fc051dd9c3e1d
Arpan Dey <midecl.arpan@gmail.com>
2024-10-16 15:14:41 +0530

    Two Swaps

---
+++ 16-10-24.cpp
@@ -0,0 +1,15 @@
+class Solution {
+  public:
+    bool checkSorted(vector<int> &arr) {
+        int count=0;
+        for(int i=0;i<arr.size();i++) {
+            if(arr[i]!=i+1) {
+                swap(arr[i],arr[arr[i]-1]);
+                count++;
+                i--;
+            }
+            if(count>2) return false;
+        }
+        return count==2|count==0;
+    }
+};
