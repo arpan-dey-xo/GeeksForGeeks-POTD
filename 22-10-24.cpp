77deee92709c08e9b3f65c3027a014384ec350bf
Arpan Dey <midecl.arpan@gmail.com>
2024-10-22 20:42:42 +0530

    Sub-Arrays With Equal Number of Occurences

---
+++ 22-10-24.cpp
@@ -0,0 +1,21 @@
+class Solution {
+  public:
+    int sameOccurrence(vector<int>& arr, int x, int y) {
+        unordered_map<int,int>ump;
+        ump[0]=1;
+        int xCount=0,yCount=0,ans=0;
+        for(int i=0;i<arr.size();i++){
+            
+            if(arr[i]==x) xCount++;
+            
+            if(arr[i]==y) yCount++;
+            
+            if(ump.find(xCount-yCount)!=ump.end()){
+                ans+=ump[xCount-yCount];
+            }
+            
+            ump[xCount-yCount]++;
+        }
+        return ans;
+    }
+};
