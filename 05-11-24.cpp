ed71310eb4ac922dfc6e401fb13a3a33932ddd50
Arpan Dey <midecl.arpan@gmail.com>
2024-11-05 19:14:22 +0530

    Rotate by 90 Degree

---
+++ 05-11-24.cpp
@@ -0,0 +1,11 @@
+void rotate(vector<vector<int>>& mat) {
+    int n = mat.size();
+    for(int i = 0; i < n ; i++) {
+        for(int j = i; j < n; j++) {
+            swap(mat[i][j], mat[j][i]);
+        }
+    }
+    for(int i = 0; i < n ; i++) {
+        reverse(mat[i].begin(), mat[i].end());
+    }
+}
