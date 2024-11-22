f673083f3fb1e77bd004b5edc676d00441909934
Arpan Dey <midecl.arpan@gmail.com>
2024-11-22 23:30:09 +0530

    Stock Buy and Sell - Max One Transaction Allowed

---
+++ 22-11-24.cpp
@@ -0,0 +1,16 @@
+class Solution {
+  public:
+    int maximumProfit(vector<int> &prices) {
+        int maxCount = 0;
+        int buyPrice = prices[0];
+        for (int i = 1; i < prices.size(); i++) {
+            if (prices[i] > buyPrice) {
+                maxCount = max(prices[i] - buyPrice, maxCount);
+            }
+            else {
+                buyPrice = prices[i];
+            } 
+        }
+        return maxCount;
+    }
+};
