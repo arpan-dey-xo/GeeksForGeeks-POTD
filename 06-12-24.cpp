fb77f83a539e4a9fb012b83254c28d8d28b17b5b
Arpan Dey <midecl.arpan@gmail.com>
2024-12-06 21:54:09 +0530

    Find H-Index

---
+++ 06-12-24.cpp
@@ -0,0 +1,14 @@
+class Solution {
+  public:
+    int hIndex(vector<int>& citations) {
+        sort(citations.begin(), citations.end());
+        int citation = 0;
+        int n = citations.size();
+        for(int i= n-1;i>=0;i--)
+        {
+            int a = min(citations[i],n-i);
+            citation = max(citation, a);
+        }
+        return citation;
+    }
+};
