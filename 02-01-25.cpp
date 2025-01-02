576564f7b6aef14a982cc98e593921e149531fd2
Arpan Dey <midecl.arpan@gmail.com>
2025-01-02 19:21:24 +0530

    Subarrays With Sum K

---
+++ 02-01-25.cpp
@@ -0,0 +1,16 @@
+class Solution {
+  public:
+    int countSubarrays(vector<int> &arr, int k) {
+        map<int, int>mp;
+        mp[0] = 1;
+        int preSum = 0, cnt = 0;
+        for(int i=0; i<arr.size(); i++)
+        {
+            preSum += arr[i];
+            int remove = preSum - k;
+            cnt += mp[remove];
+            mp[preSum] += 1;
+        }
+        return cnt;
+    }
+};
