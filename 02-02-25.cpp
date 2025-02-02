a707094750f6cd99f2ff705712bcf5e21a68d401
Arpan Dey <midecl.arpan@gmail.com>
2025-02-02 21:11:35 +0530

    Level Order Traversal

---
+++ 02-02-25.cpp
@@ -0,0 +1,27 @@
+class Solution {
+  public:
+    vector<vector<int>> levelOrder(Node *root) {
+        vector<vector<int>> levelOrderStorage;
+        
+        queue<Node*> q;
+        q.push(root);
+        
+        while(!q.empty()){
+            int n = q.size();
+            vector<int> temp;
+            while(n--){
+                Node* topEle = q.front();
+                q.pop();
+                
+                temp.push_back(topEle->data);
+                if(topEle->left)    
+                    q.push(topEle->left);
+                if(topEle->right)
+                    q.push(topEle->right);    
+            }
+            levelOrderStorage.push_back(temp);
+        }
+        
+        return levelOrderStorage;
+    }
+};
