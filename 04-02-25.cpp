1333df8a0f2ed982d37066c0458ef1dd07454864
Arpan Dey <midecl.arpan@gmail.com>
2025-02-04 20:33:59 +0530

    Diameter of Binary Tree

---
+++ 04-02-25.cpp
@@ -0,0 +1,22 @@
+class Solution {
+  public:
+    int solve(Node* node,int &d){
+        if(!node)
+        return 0;
+        
+        int l=solve(node->left,d);
+        int r=solve(node->right,d);
+        
+        d=max(d,l+r+1);
+        
+        return 1 + max(l,r);
+    }
+    
+    int diameter(Node* root) {
+        int d=0;
+        solve(root,d);
+        
+        return d-1;
+        
+    }
+};
