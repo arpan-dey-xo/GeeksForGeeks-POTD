ebab0e8d49a9d5bdc754be63f9dbf2b0b07068b0
Arpan Dey <midecl.arpan@gmail.com>
2025-02-10 18:20:44 +0530

    K Sum Paths

---
+++ 10-02-25.cpp
@@ -0,0 +1,30 @@
+class Solution {
+    public:
+      int ans = 0;
+      void solve(Node* root,int k,int sum,unordered_map<int,int> &mp) {
+          if(root == NULL) {
+              return ;
+          }
+          
+          sum += root->data;
+          
+          if(mp.find(sum-k) != mp.end()) 
+              ans += mp[sum-k];
+              
+              mp[sum]++;
+              
+              solve(root->left,k,sum,mp);
+              solve(root->right,k,sum,mp);
+              
+              mp[sum]--;
+          
+      }
+      int sumK(Node *root, int k) {
+          unordered_map<int,int> mp;
+          mp[0] = 1;
+          
+          solve(root,k,0,mp);
+          
+          return ans;
+      }
+  };
