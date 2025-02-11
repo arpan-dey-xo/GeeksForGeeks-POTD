614ef7f210dde74dec7220325808967d91cfece5
Arpan Dey <midecl.arpan@gmail.com>
2025-02-11 21:32:23 +0530

    Check for BST

---
+++ 11-02-25.cpp
@@ -0,0 +1,21 @@
+class Solution {
+    public:
+    void inorder(Node* root,vector<int> &ans){
+        if(root==NULL){
+            return;
+        }
+        inorder(root->left,ans);
+        ans.push_back(root->data);
+        inorder(root->right,ans);
+    }
+    bool isBST(Node* root) {
+        vector<int> ans;
+        inorder(root,ans);
+        for(int i=0;i<ans.size()-1;i++){
+            if(ans[i]>=ans[i+1]){
+                return false;
+            }
+        }
+        return true;
+    }
+};
