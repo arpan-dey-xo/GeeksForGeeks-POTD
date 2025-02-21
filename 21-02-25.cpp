a216fbd52967c67dc006fdf52e99f30d18b24cc1
Arpan Dey <midecl.arpan@gmail.com>
2025-02-21 20:12:59 +0530

    Parenthesis Checker

---
+++ 21-02-25.cpp
@@ -0,0 +1,19 @@
+class Solution {
+    public:
+      bool isBalanced(string& x) {
+          unordered_map<char,char> mm;
+          mm[')']='(';
+          mm[']']='[';
+          mm['}']='{';
+          stack<char> st;
+          for(auto c:x){
+              if(c=='(' or c=='[' or c=='{')st.push(c);
+              else if(st.empty())return false;
+              else{
+                  if(st.top()!=mm[c])return false;
+                  st.pop();
+              }
+          }
+          return st.empty();
+      }
+  };
