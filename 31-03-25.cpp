d1b338d08c76bafc9764d7c9441c945e5f284f1c
Arpan Dey <midecl.arpan@gmail.com>
2025-03-31 22:14:40 +0530

    Maximize Partitions in a String

---
+++ 31-03-25.cpp
@@ -0,0 +1,18 @@
+class Solution {
+    public:
+      int maxPartitions(string &s) {
+          int n=s.size();
+          unordered_map<char,int>mp;
+          for(int i=0;i<n;i++) {
+              mp[s[i]]=i;
+          }
+          int end=0,cnt=0;;
+          for(int i=0;i<n;i++) {
+              end=max(mp[s[i]],end);
+              if(i==end) {
+                  cnt++;
+              }
+          }
+          return cnt;
+      }
+  };
