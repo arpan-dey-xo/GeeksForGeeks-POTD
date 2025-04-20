e586f61b89f5333c2d6bee0e9af8bfda243cf4f9
Arpan Dey <midecl.arpan@gmail.com>
2025-04-20 21:10:13 +0530

    Find Only Repetitive Element from 1 to n-1

---
+++ 20-04-25.cpp
@@ -0,0 +1,11 @@
+class Solution {
+    public:
+      int findDuplicate(vector<int>& arr) {
+          int ans=0;
+          for(int i=1;i<arr.size();i++)
+              ans ^= i;
+          for(auto &ele: arr)
+              ans ^= ele;
+          return ans;
+      }
+  };
