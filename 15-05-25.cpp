1254b4100b48abc5e83c58eedcc4a6d6bc82634e
Arpan Dey <midecl.arpan@gmail.com>
2025-05-15 21:49:22 +0530

    Substrings with Same First and Last Characters

---
+++ 15-05-25.cpp
@@ -0,0 +1,11 @@
+class Solution {
+  public:
+    int countSubstring(string &s) {
+        int n=s.length(), count = 0, a[26] = {0};
+        for(int i = 0; i < n; i++) {
+            a[s[i] - 'a']++;
+            count += a[s[i] - 'a'];
+        }
+        return count;
+    }
+};
