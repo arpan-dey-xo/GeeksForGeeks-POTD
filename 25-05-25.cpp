b0bd4c5b530c7a0202379fefac2da824938daa66
Arpan Dey <midecl.arpan@gmail.com>
2025-05-25 22:30:15 +0530

    Pythagorean Triplet

---
+++ 25-05-25.cpp
@@ -0,0 +1,25 @@
+class Solution {
+  public:
+    bool pythagoreanTriplet(vector<int>& arr) {
+        int n=arr.size();
+        int maxi=*max_element(arr.begin(),arr.end());
+        vector<int>elements(maxi+1,0);
+        for(int i:arr){
+            elements[i]++;
+        }
+        
+        for(int i=1;i<=maxi;i++){
+            for(int j=i+1;j<=maxi;j++){
+                if(elements[i] && elements[j]){
+                    int a=i,b=j;
+                    int right=(a*a) + (b*b);
+                    int c=sqrt(right);
+                    if(c<=maxi && (c*c)==right && elements[c]) 
+                        return true;
+                }
+            }
+        }
+        
+        return false;
+    }
+};
