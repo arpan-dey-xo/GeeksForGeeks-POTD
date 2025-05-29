934230ff3b4efb0f050e46d560871ce641c9ae7a
Arpan Dey <midecl.arpan@gmail.com>
2025-05-29 19:47:03 +0530

    Sum of Nodes on the Longest Path

---
+++ 29-05-25.cpp
@@ -0,0 +1,26 @@
+class Solution {
+  public:
+    int maxLen, maxSum;
+    void solve(Node* node, int len, int sum){
+        if(!node) return;
+        len+=1;
+        sum+=node->data;
+        if(!node->left && !node->right){
+            if(len>maxLen){
+                maxSum=sum;
+                maxLen=len;
+            }else if(len==maxLen){
+                maxSum=max(maxSum, sum);
+            }
+            return;
+        }
+        solve(node->left, len, sum);
+        solve(node->right, len, sum);
+    }
+  
+    int sumOfLongRootToLeafPath(Node *root) {
+        maxLen=0, maxSum=0;
+        solve(root, 0, 0);
+        return maxSum;
+    }
+};
