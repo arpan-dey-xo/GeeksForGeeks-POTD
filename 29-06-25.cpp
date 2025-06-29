ba4df4274a066f9d8bd06a4cbb41bb6280056866
Arpan Dey <midecl.arpan@gmail.com>
2025-06-29 22:50:37 +0530

    Split Array Largest Sum

---
+++ 29-06-25.cpp
@@ -0,0 +1,34 @@
+class Solution {
+  public:
+    int splitArray(vector<int>& arr, int k) {
+        int left = *max_element(arr.begin(), arr.end());
+        int right = accumulate(arr.begin(), arr.end(), 0);
+        
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (isFeasible(arr, k, mid)) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+    
+  private:
+    bool isFeasible(const vector<int>& arr, int k, int maxSum) {
+        int sum = 0;
+        int count = 1;
+        for (int num : arr) {
+            sum += num;
+            if (sum > maxSum) {
+                sum = num;
+                count++;
+                if (count > k) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
